d277f0379a31aadb0085de0829b855a9ffa35e70
PJ <petejones398@gmail.com>
2020-05-21 11:55:56 +0100

    move error check inside qread

--- R/read_data.R
+++ R/read_data.R
@@ -17,12 +17,19 @@
 #'   characters in the events list.
 #' @param start_at The column containing the sender IDs, followed by dummy
 #'   variables for each character.
+#' @param check_errors If TRUE, the function checks for common data input
+#'   errors. It looks for characters with self-ties, empty rows where no
+#'   recipient was inputted, and any values other than 1 and 0 in the recipient
+#'   dummy columns. Returns a message reporting the results of the checks.
 #'
 #' @return A named list containing the event list, node list and an adjacency
 #'   matrix derived from the event list.
 #'
 #' @export
-qread_film <- function(events_file, nodes_file, start_at = 3) {
+qread_film <- function(events_file,
+                       nodes_file,
+                       start_at = 3,
+                       check_errors = TRUE) {
   # First, let's read in the event list and the node list
   lines <- read.csv(events_file, sep = ',', stringsAsFactors = FALSE)
   # Now, let's read in the node list and add some attributes we'll want later
@@ -53,60 +60,37 @@ qread_film <- function(events_file, nodes_file, start_at = 3) {
     rownames(adj) <- rownames(lines)[start_at + 1:ncol(lines)]
   }
 
-  return(list(event_list = lines, node_list = chars, adjacency = adj))
-}
-
-#' Check for easily identifiable data input errors
-#'
-#' @description This helper function checks for common data input errors. It
-#'   looks for characters with self-ties, empty rows where no recipient was
-#'   inputted, and any values other than 1 and 0.
-#'
-#' @param adjacency The adjacency matrix returned from data read-in.
-#' @param event_list The event list returned from data read-in.
-#' @param start_at Where to start checking for unexpected values. Important to
-#'   note that unlike other functions in the package, start_at should be set to
-#'   the first recipient dummy column in the data, which is set by default to 4.
-#'
-#' @return A message reportinf the results of the check.
-#'
-#' @export
-check_for_errors <- function(adjacency = NULL, event_list = NULL,
-                             start_at = 4) {
-  if(is.null(adjacency)) {
-    stop("Please provide an adjacency matrix via the 'adjacency' argument")
-  }
-  if(is.null(event_list)) {
-    stop("Please provide an event list via the 'event_list' argument")
-  }
-
-  self_ties <- vector("numeric", length = nrow(adjacency))
-  # Check diagonal for self-ties
-  for (i in 1:nrow(adjacency)){
-    if(adjacency[i, i] > 0) {
-      self_ties[i] <- 1
+  if(check_errors == TRUE) {
+    self_ties <- vector("numeric", length = nrow(adj))
+    # Check diagonal for self-ties
+    for (i in 1:nrow(adj)){
+      if(adj[i, i] > 0) {
+        self_ties[i] <- 1
+      } else {
+        self_ties[i] <- 0
+      }
+    }
+    if(length(which(self_ties > 0)) > 0) {
+      cat("Characters with self-ties: ", which(self_ties > 0))
     } else {
-      self_ties[i] <- 0
+      cat("No characters with self-ties found.")
+    }
+    cat("\n")
+    # Check for empty rows (no recipients indicated)
+    if(length(which(rowSums(lines[ , (start_at + 1):ncol(lines)]) == 0)) > 0)
+    {
+      cat("Empty rows: ",
+          which(rowSums(lines[ , (start_at + 1):ncol(lines)]) == 0))
+    } else {
+      cat("No empty rows found.")
+    }
+    # Check for other data entry errors (cell values not in c(0, 1))
+    if(FALSE %in% unique(c(as.matrix(lines)[ , (start_at + 1):ncol(lines)]))
+       %in% c("0", "1")) {
+      cat("\nData entry values: ",
+          unique(c(as.matrix(lines)[ , (start_at + 1):ncol(lines)])))
     }
   }
-  if(length(which(self_ties > 0)) > 0) {
-    cat("Characters with self-ties: ", which(self_ties > 0))
-  } else {
-    cat("No characters with self-ties found.")
-  }
-  cat("\n")
-  # Check for empty rows (no recipients indicated)
-  if(length(which(rowSums(event_list[ , start_at:ncol(event_list)]) == 0)) > 0)
-  {
-    cat("Empty rows: ",
-        which(rowSums(event_list[ , start_at:ncol(event_list)]) == 0))
-  } else {
-    cat("No empty rows found.")
-  }
-  # Check for other data entry errors (cell values not in c(0, 1))
-  if(FALSE %in% unique(c(as.matrix(event_list)[ , start_at:ncol(event_list)]))
-     %in% c("0", "1")) {
-    cat("\nData entry values: ",
-        unique(c(as.matrix(event_list)[ , start_at:ncol(event_list)])))
-  }
+
+  return(list(event_list = lines, node_list = chars, adjacency = adj))
 }
