c7413852219021c33aa6c44d18bdfa422005cfc7
PJ <petejones398@gmail.com>
2021-02-19 13:57:50 +0000

    add functions for reshaping between dyadic and multicast

--- R/helpers.R
+++ R/helpers.R
@@ -144,3 +144,139 @@ check_sugs <- function(suggest) {
     }
   }
 }
+
+#' Convert a dyadic event list to multicast format
+#'
+#' @description This function converts a time-ordered event list in a dyadic
+#'   edge-list style format with a 'from' column and a 'to' column into a
+#'   multicast format as used by this package with receiver dummy variables
+#'   after the from column. If the dyadic event list contains a column
+#'   indicating the time or event ID, this can be specified to collect multiple
+#'   simultaneous dyadic events spread across multiple rows into a single row in
+#'   the multicast event list.
+#'
+#' @param event_list A time-ordered dyadic event list.
+#' @param from The numerical index of the 'from' column in \code{event_list}.
+#' @param to The numerical index of the 'to' column in \code{event_list}.
+#' @param time_col The numerical index of the column in \code{event_list}
+#'   containing the event or time IDs. This is used to identify where multiple
+#'   dyadic events are actually part of the same multicast interaction. If no
+#'   such column exists, or all events are truly dyadic and sequential, the
+#'   function will simply treat each row as an event.
+#' @param char_names An optional character vector of character names, ordered by character
+#'   ID, used to label the columns.
+#'
+#' @return A time-ordered multicast event list as a matrix.
+#'
+#' @examples
+#' tfa <- movienetdata::starwars_01
+#' # Convert The Force Awakens event list to dyadic format
+#' tfa_dyads <- multicast_to_dyadic(tfa$event_list,
+#'                                  from = 3)
+#' # And convert it back to multicast format
+#' tfa_events <- dyadic_to_multicast(event_list = tfa_dyads,
+#'                                   from = 4,
+#'                                   to = 5,
+#'                                   time_col = 1,
+#'                                   char_names = tfa$node_list$char_name)
+#'
+#' @export
+dyadic_to_multicast <- function(event_list,
+                                from = 1,
+                                to = 2,
+                                time_col = NULL,
+                                char_names = NULL) {
+  nchars <- length(unique(c(event_list[ , from], event_list[ , to])))
+  if(is.numeric(event_list[ , from]) & is.numeric(event_list[ , to])) {
+    fromID <- event_list[ , from]
+    toID <- event_list[ , to]
+  } else {
+    fromID <- numeric(length = nrow(event_list))
+    toID <- numeric(length = nrow(event_list))
+    chars <- unique(c(event_list[ , from], event_list[ , to]))
+    for(i in 1:nrow(event_list)) {
+      fromID[i] <- which(chars == event_list[i, from])
+      toID[i] <- which(chars == event_list[i, to])
+    }
+  }
+
+  if(is.null(time_col)) {
+    timeID <- seq.int(from = 1, to = nrow(event_list))
+  } else {
+    timeID <- event_list[ , time_col]
+  }
+  multicast <- matrix(0, nrow = length(unique(timeID)), ncol = nchars + 2)
+  if(!is.null(char_names)) {
+    colnames(multicast) <- c("eventID", "from", char_names)
+  }else {
+    if(is.numeric(event_list[ , from]) & is.numeric(event_list[ , to])) {
+      colnames(multicast) <- c("eventID", "from", paste0("Character ",
+                                                         1:nchars))
+    } else {
+      colnames(multicast) <- c("eventID", "from", chars)
+    }
+  }
+
+  for (i in 1:nrow(event_list)) {
+    event_id <- timeID[i]
+    multicast[event_id, 1] <- event_id
+    multicast[event_id, 2] <- fromID[i]
+    multicast[event_id, (toID[i] + 2)] <- 1
+  }
+  return(multicast)
+}
+
+#' Convert a multicast event list to dyadic format
+#'
+#' @description Convert a time-ordered multicast event list into a dyadic
+#'   from-to format, preserving other event-level variables.
+#'
+#' @param event_list A multicast event list containing a column of sender IDs
+#'   followed immediately by dummy variables for each character.
+#' @param from The column containing the sender IDs.
+#' @param force_seq Logical. If TRUE, events with multiple recipients will be
+#'   forced into a false sequence, and each row in the dyadic event list will
+#'   increment the event ID variable. If FALSE, multicast events will be split
+#'   into the dyadic components, but the event ID variable will indicate that
+#'   these dyadic events are simultaneous.
+#'
+#' @return A time-ordered dyadic event list (as a matrix).
+#'
+#' @examples
+#' tfa <- movienetdata::starwars_01
+#' # Convert The Force Awakens event list to dyadic format
+#' tfa_dyads <- multicast_to_dyadic(tfa$event_list,
+#'                                  from = 3)
+#' # And convert it back to multicast format
+#' tfa_events <- dyadic_to_multicast(event_list = tfa_dyads,
+#'                                   from = 4,
+#'                                   to = 5,
+#'                                   time_col = 1,
+#'                                   char_names = tfa$node_list$char_name)
+#'
+#' @export
+multicast_to_dyadic <- function(event_list,
+                                from = 3,
+                                force_seq = FALSE) {
+  events_data <- matrix(0,
+                        nrow = sum(event_list[ , (from + 1):ncol(event_list)]),
+                        ncol = from + 2)
+  k <- 0
+  for (i in 1:nrow(event_list)) {
+    for (j in (from + 1):ncol(event_list)) {
+      if(event_list[i, j] == 1) {
+        k <- k + 1
+        if(force_seq == TRUE) {
+          events_data[k, ] <- c(k, event_list[i, 1:from], j - from)
+        } else {
+          events_data[k, ] <- c(i, event_list[i, 1:from], j - from)
+        }
+      } else {
+        next
+      }
+    }
+  }
+  colnames(events_data) <- c("time", dimnames(event_list)[[2]][1:from - 1],
+                             "from", "to")
+  return(events_data)
+}
